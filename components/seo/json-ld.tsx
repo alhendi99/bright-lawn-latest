3eff2e3021b726e22900e68d4682dd299dff73b4
alhendi99 <hendiamrak@gmail.com>
2026-08-07 06:11:38 +0300

    Fix formatting in JsonLd component script type declaration

--- components/seo/json-ld.tsx
+++ components/seo/json-ld.tsx
@@ -5,7 +5,7 @@ type JsonLdProps = {
 export function JsonLd({ data }: JsonLdProps) {
   return (
     <script
-      type="application/ld+json"
+      type="application/ld+json" 
       dangerouslySetInnerHTML={{
         __html: JSON.stringify(data).replace(/</g, '\\u003c'),
       }}
